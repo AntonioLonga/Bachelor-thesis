152ee33ecd7603652b944dd9df9f13219f894962
AntonioLonga94 <longaantonio@gmail.com>
2017-07-06 19:12:39 +0200

    dalla classe script richiami la classe FindIdentifier

--- checkObfuscation/FindIdentifier.py
+++ checkObfuscation/FindIdentifier.py
@@ -2,94 +2,65 @@ import sys
 import re
 import os
 
-res=[]
 
-#cerca ricorsivamente all'interno di tutte le catelle e se trova un file.smali
-#chiama il metodo searchInsideFile
-def searchSmaliCode(path):
+class FindIdentifier:
+    res=[]
 
-    if (os.path.isdir(path)):
-        files=os.listdir(path)
+    #cerca ricorsivamente all'interno di tutte le catelle e se trova un file.smali
+    #chiama il metodo searchInsideFile
+    def searchSmaliCode(self,path):
+        
+        if (os.path.isdir(path)):
+            files=os.listdir(path)
 
-        for element in files:
-            if (re.match("\w*.smali",element)):
-                print ("smali: "+element)
-                searchInsideFile(path+"/"+element)
-            else:
-                searchSmaliCode(path+"/"+element)
+            for element in files:
+                if (re.match("\w*.smali",element)):
+                    print ("smali: "+element)
+                    self.searchInsideFile(path+"/"+element)
+                else:
+                    self.searchSmaliCode(path+"/"+element)
 
 
 
     
-def searchInsideFile(smalipath):
-    """Return name of variables, calsses and method.
-    This method take as input the path of smali file, and return an array of names
-    """
-    #trova i metodi,calssi,variabile
-    patternMetodo=".method (\w*)? (\w*)"
-    patternVariabile="(\s*)?(.local )(\w*), \"(\w*)\".*"
-    patternClasse=".source \"(\w*).java\""
+    def searchInsideFile(self,smalipath):
+        """Return name of variables, calsses and method.
+        This method take as input the path of smali file, and return an array of names
+        """
+        #trova i metodi,calssi,variabile
+        patternMetodo=".method (\w*)? (\w*)"
+        patternVariabile="(\s*)?(.local )(\w*), \"(\w*)\".*"
+        patternClasse=".source \"(\w*).java\""
 
-    
-    with open(smalipath) as infile:
-        for line in infile:
-            method=re.match(patternMetodo,line)
-            variable=re.match(patternVariabile,line)
-            classe=re.match(patternClasse,line)
-            if (method and not(method.group(2)=="constructor" or method.group(2)=="static" or method.group(2)=="abstract" or method.group(2)=="final")):
-                print ("nome del metodo: "+method.group(2))
-                res.append(method.group(2))
-            if variable:
-                print ("nome della variabile: "+variable.group(4))
-                res.append(variable.group(4))
-            if classe:
-                print ("nome della classe: "+classe.group(1))
-                res.append(classe.group(1))
+        
+        with open(smalipath) as infile:
+            for line in infile:
+                method=re.match(patternMetodo,line)
+                variable=re.match(patternVariabile,line)
+                classe=re.match(patternClasse,line)
+                if (method and not(method.group(2)=="constructor" or method.group(2)=="static" or method.group(2)=="abstract" or method.group(2)=="final")):
+                    print ("nome del metodo: "+method.group(2))
+                    self.res.append(method.group(2))
+                if variable:
+                    print ("nome della variabile: "+variable.group(4))
+                    self.res.append(variable.group(4))
+                if classe:
+                    print ("nome della classe: "+classe.group(1))
+                    self.res.append(classe.group(1))
+    def start(self,path):
+        folder=os.listdir(path)
 
+        
+        #verifico che all'interno del percorso ci sia una catella smali e
+        #che dentro di lei ci sia la catella com
 
-
-
-#prendo l'input temporaneamente
-#la cartella che contiene la cartella smali....tipo
-#../test1/test1.2/
-
-
-path=sys.argv[1]
-folder=os.listdir(path)
-
-#verifico che all'interno del percorso ci sia una catella smali e
-#che dentro di lei ci sia la catella com
-
-for element in folder:
-    if (element=="smali"):
-        path=path+"/smali"
-        subfolder=os.listdir(path)
-        for elem in subfolder:
-            if (elem=="com"):
-                searchSmaliCode(path+"/com")
+        for element in folder:
+            if (element=="smali"):
+                path=path+"/smali"
+                subfolder=os.listdir(path)
+                for elem in subfolder:
+                    if (elem=="com"):
+                       self.searchSmaliCode(path+"/com")
 
 
-print(res)
-
-    
-
-
-
-
-
-
-
-
-
-
-"""
-for element in smaliFiles:
-    if not(element[0]=="R" and element[1]=="$"):
-        if not(element=="R.smali" or element=="BuildConfig.smali"):
-            searchInsideFile(path+"/"+element)
- """ 
-
-
-
-
-
+        print(self.res)
---
+++ checkObfuscation/script.py
@@ -0,0 +1,21 @@
+from FindIdentifier import FindIdentifier
+import sys
+import re
+import os
+
+
+#prendo l'input temporaneamente
+#la cartella che contiene la cartella smali....tipo
+#../test1/test1.2/
+
+
+path=sys.argv[1]
+
+
+
+
+
+FindIdentifier().start(path)
+
+
+
